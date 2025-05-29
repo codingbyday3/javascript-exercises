33eb794270a7d3652fa7d34d12235e60a0a601b7
Klemen <klemen.bussines12@gmail.com>
2025-05-29 22:43:16 +0200

    Build: function which remove specific item from array
    My way is not that good I found out that this
    function can be wrote alot better with forEach
    or map method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function (arr, ...numbersToRemove) {
+  for (let num of numbersToRemove) {
+    while(arr.includes(num)){
+      arr.splice(arr.indexOf(num), 1);
+    }
+  }
+  return arr
+}
 // Do not edit below this line
 module.exports = removeFromArray;
