c225a32556419ad88077190128aa9d77682065dd
Klemen <klemen.bussines12@gmail.com>
2025-05-30 08:49:20 +0200

    Build: function which sum numbers between two numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  sum = 0
+  if(start < 0 || end < 0){
+      return "ERROR"
+  }else if(!Number.isInteger(start) || !Number.isInteger(end)){
+      return "ERROR"
+  }else if(start > end){
+    let temp = start
+    start = end
+    end = temp
+  }
 
+  for(let i = start; i <= end; i++){
+    sum += i
+  }
+
+  return sum
 };
 
 // Do not edit below this line
