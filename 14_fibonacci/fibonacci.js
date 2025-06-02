8babe7829dbefe0c8181c9f30453a4b71610ddfd
Klemen <klemen.bussines12@gmail.com>
2025-06-02 19:45:56 +0200

    Feat: return fibbonaci member of your choice

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(numOfMember) {
+  let fibonacciArr = [1]
+  let fibonacciNum = 0
+  let i = 1
 
+  if(numOfMember < 0){
+    return "OOPS"
+  }
+
+  while(i <= numOfMember){
+    i++
+    fibonacciArr.push(fibonacciNum)
+  
+    fibonacciNum += (fibonacciArr[fibonacciArr.length - 2]) 
+    
+  }
+
+  return fibonacciNum
 };
 
 // Do not edit below this line
