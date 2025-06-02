95239c6aba0bbb6eb08caca909a9fd25e9760f40
Klemen <klemen.bussines12@gmail.com>
2025-06-02 20:48:45 +0200

    Feat: display the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  getAge(people)
+  return people.sort((a, b) => b.age - a.age)[0]
+  
+}
 
-};
+function getAge(people){
+  death = new Date().getFullYear()
+  people.forEach((man) =>{ 
+
+  if(man.yearOfDeath){
+    man["age"] = man.yearOfDeath - man.yearOfBirth
+  }else{
+    man["age"] = death - man.yearOfBirth
+  }
+  })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
