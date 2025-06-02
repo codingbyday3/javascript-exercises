e23fae2ead97d4c0cb32d8aff5c56001fb8af48a
Klemen <klemen.bussines12@gmail.com>
2025-06-02 19:18:20 +0200

    Feat: palindrome search

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+  palindrome = palindrome.toLowerCase();
+  backwardWord = palindrome
+    .split('')
+    .reverse()
+    .join('')
+    .replace(/[^a-z0-9]/gi, '');
+
+ 
+  return backwardWord === palindrome.replace(/[^a-z0-9]/gi, '')
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
