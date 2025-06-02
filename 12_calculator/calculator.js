47ad2c2448d80d6561bc23d376e33d70f0d5e014
Klemen <klemen.bussines12@gmail.com>
2025-06-02 18:40:09 +0200

    Make: calculator exercice

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arrOfNum) {
+	return arrOfNum.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arrOfNum) {
+  return arrOfNum.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  total = 1
+	while(num !== 0){
+    total *= num
+    num--
+  }
+  return total
 };
 
 // Do not edit below this line
